044138c1fc865e7246bbfb6f8004050ed554778f
feisalmombo <feisalmombo29@gmail.com>
2023-08-14 12:17:41 +0300

    update new-footer blade file

---
+++ resources/views/partials/new-footer.blade.php
@@ -0,0 +1,36 @@
+<footer class="footer" role="contentinfo">
+    <div class="footer-top">
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12  col-md-4">
+            <div class="widget  widget_black_studio_tinymce">
+                <div class="textwidget">
+                    {{--  <p><img alt="Legend Logistics" width="351" height="108" data-src="#" class="alignnone wp-image-786 lazyload" src="#" alt="Legend Logistics Company Limited">
+                </p>  --}}
+                <h1 style="color: white">Legend Logistics</h1>
+<p>&nbsp;</p>
+</div></div></div><div class="col-xs-12  col-md-2"><div class="widget  widget_nav_menu"><h6 class="footer-top__headings">Our Services</h6><div class="menu-services-menu-container"><ul id="menu-services-menu" class="menu"><li id="menu-item-111" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-111"><a href="services.html">All Services</a></li>
+<li id="menu-item-112" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-112"><a href="ground-transport.html">Land Freight Services</a></li>
+<li id="menu-item-113" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-113"><a href="cargo.html">Freight Services</a></li>
+<li id="menu-item-114" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-114"><a href="warehousing.html">Cargo Insurance</a></li>
+<li id="menu-item-117" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-117"><a href="storage.html">Customs Clearance Services</a></li>
+</ul></div></div></div><div class="col-xs-12  col-md-2"><div class="widget  widget_black_studio_tinymce"><h6 class="footer-top__headings">Address</h6><div class="textwidget"><p>MIC Building, 1st Floor,<br>
+Dar-Tanzania<br>
+P. O Box 3194<br>
+Mob: +255684456287<br>
+Email:&nbsp;<a href="mailto:info@legendlogistics.co.tz" target="_blank" rel="noopener">info@legendlogistics.co.tz </a></p>
+</div></div></div><div class="col-xs-12  col-md-4"><div class="widget  widget_black_studio_tinymce"><div class="textwidget"><p><a class="icon-container  icon-container--square" href="" target="_self"><span class="fa fa-facebook"></span></a>&nbsp; &nbsp;<a class="icon-container  icon-container--square" href="" target="_blank"><span class="fa fa-instagram"></span></a> <a class="icon-container  icon-container--square" href="" target="_blank"><span class="fa fa-linkedin"></span></a> <a class="icon-container  icon-container--square" href="" target="_blank"><span class="fa fa-skype"></span></a> <a class="btn btn-primary" href="#" target="_blank">Contact Us Now</a></p>
+</div></div></div>				</div>
+        </div>
+    </div>
+            <div class="footer-bottom">
+        <div class="container">
+            <div class="footer-bottom__left">
+                Developed by  <a href="#">Feisal Suleiman Mombo</a>
+            </div>
+            <div class="footer-bottom__right">
+                Copyright &copy; {{ date('Y') }} Legend Logistics. All rights reserved.
+            </div>
+        </div>
+    </div>
+</footer>
