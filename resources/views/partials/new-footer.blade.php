91e575590f2c06ebcc6258221cc27a0c7f192152
feisalmombo <feisalmombo29@gmail.com>
2023-08-14 12:52:59 +0300

    WIP

--- resources/views/partials/new-footer.blade.php
+++ resources/views/partials/new-footer.blade.php
@@ -10,10 +10,11 @@
                 <h1 style="color: white">Legend Logistics</h1>
 <p>&nbsp;</p>
 </div></div></div><div class="col-xs-12  col-md-2"><div class="widget  widget_nav_menu"><h6 class="footer-top__headings">Our Services</h6><div class="menu-services-menu-container"><ul id="menu-services-menu" class="menu"><li id="menu-item-111" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-111"><a href="services.html">All Services</a></li>
-<li id="menu-item-112" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-112"><a href="ground-transport.html">Land Freight Services</a></li>
-<li id="menu-item-113" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-113"><a href="cargo.html">Freight Services</a></li>
-<li id="menu-item-114" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-114"><a href="warehousing.html">Cargo Insurance</a></li>
-<li id="menu-item-117" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-117"><a href="storage.html">Customs Clearance Services</a></li>
+<li id="menu-item-112" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-112"><a href="#">Road Transport</a></li>
+<li id="menu-item-112" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-112"><a href="#">Air Freight</a></li>
+<li id="menu-item-112" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-112"><a href="#">Ocean Freight</a></li>
+<li id="menu-item-113" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-113"><a href="#">Warehousing</a></li>
+<li id="menu-item-114" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-114"><a href="#">Railway</a></li>
 </ul></div></div></div><div class="col-xs-12  col-md-2"><div class="widget  widget_black_studio_tinymce"><h6 class="footer-top__headings">Address</h6><div class="textwidget"><p>MIC Building, 1st Floor,<br>
 Dar-Tanzania<br>
 P. O Box 3194<br>
@@ -26,7 +27,7 @@ Email:&nbsp;<a href="mailto:info@legendlogistics.co.tz" target="_blank" rel="noo
             <div class="footer-bottom">
         <div class="container">
             <div class="footer-bottom__left">
-                Developed by  <a href="#">Feisal Suleiman Mombo</a>
+                Developed by  <a href="#">EFE TECH CO.LTD</a>
             </div>
             <div class="footer-bottom__right">
                 Copyright &copy; {{ date('Y') }} Legend Logistics. All rights reserved.
