b0082d8f9bc2f5eef970ff023d89c544984c0b79
feisalmombo <feisalmombo29@gmail.com>
2023-08-15 12:35:56 +0300

    WIP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -266,7 +266,7 @@
 <!-- START REVOLUTION SLIDER 5.4.8.3 fullwidth mode -->
 	<div id="rev_slider_1_1" class="rev_slider fullwidthabanner" style="display:none;" data-version="5.4.8.3">
 <ul>	<!-- SLIDE  -->
-	<li data-index="rs-1" data-transition="fade" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="300" data-thumb="https://umojalogistics.co.rw/wp-content/uploads/2019/07/image1sa-ok-100x50.jpg" data-rotate="0" data-saveperformance="off" data-title="Slide" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
+	<li data-index="rs-1" data-transition="fade" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="300" data-thumb="{{asset('gfl/images/transport.jpg')}}" data-rotate="0" data-saveperformance="off" data-title="Slide" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
 		<!-- MAIN IMAGE -->
 		<img src="{{asset('gfl/images/truck.jpg')}}" alt="" title="image1sa ok" width="1920" height="672" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina>
 		<!-- LAYERS -->
