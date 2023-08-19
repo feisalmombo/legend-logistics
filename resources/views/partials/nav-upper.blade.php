8b9dcb52edf39a8cd7d78c23b50fc30730098f9f
feisalmombo <feisalmombo29@gmail.com>
2023-08-19 15:14:15 +0300

    WIP

---
+++ resources/views/partials/nav-upper.blade.php
@@ -0,0 +1,20 @@
+<div class="top">
+    <div class="container">
+        <!-- Top Tagline from WordPress -->
+        <div class="top__tagline">
+            All about Legend logistics </div>
+        <!-- Top Menu -->
+        <nav class="top__menu" role="navigation" aria-label="Top Menu">
+            <ul id="menu-top-menu" class="top-navigation  js-dropdown" role="menubar">
+                <li id="menu-item-793"
+                    class="menu-item menu-item-type-custom menu-item-object-custom menu-item-793">
+                <li id="menu-item-824"
+                    class="menu-item menu-item-type-post_type menu-item-object-page menu-item-824"><a
+                        href="{{ url('/about-us') }}">About Us</a></li>
+                <li id="menu-item-791"
+                    class="menu-item menu-item-type-post_type menu-item-object-page menu-item-791"><a
+                        href="#">Contact Us</a></li>
+            </ul>
+        </nav>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -708,10 +708,10 @@
 
     <script type="text/javascript">
         function setREVStartSize(e){
-								try{ e.c=jQuery(e.c);var i=jQuery(window).width(),t=9999,r=0,n=0,l=0,f=0,s=0,h=0;
-									if(e.responsiveLevels&&(jQuery.each(e.responsiveLevels,function(e,f){f>i&&(t=r=f,l=e),i>f&&f>r&&(r=f,n=e)}),t>r&&(l=n)),f=e.gridheight[l]||e.gridheight[0]||e.gridheight,s=e.gridwidth[l]||e.gridwidth[0]||e.gridwidth,h=i/s,h=h>1?1:h,f=Math.round(h*f),"fullscreen"==e.sliderLayout){var u=(e.c.width(),jQuery(window).height());if(void 0!=e.fullScreenOffsetContainer){var c=e.fullScreenOffsetContainer.split(",");if (c) jQuery.each(c,function(e,i){u=jQuery(i).length>0?u-jQuery(i).outerHeight(!0):u}),e.fullScreenOffset.split("%").length>1&&void 0!=e.fullScreenOffset&&e.fullScreenOffset.length>0?u-=jQuery(window).height()*parseInt(e.fullScreenOffset,0)/100:void 0!=e.fullScreenOffset&&e.fullScreenOffset.length>0&&(u-=parseInt(e.fullScreenOffset,0))}f=u}else void 0!=e.minHeight&&f<e.minHeight&&(f=e.minHeight);e.c.closest(".rev_slider_wrapper").css({height:f})
-								}catch(d){console.log("Failure at Presize of Slider:"+d)}
-							};
+                try{ e.c=jQuery(e.c);var i=jQuery(window).width(),t=9999,r=0,n=0,l=0,f=0,s=0,h=0;
+                    if(e.responsiveLevels&&(jQuery.each(e.responsiveLevels,function(e,f){f>i&&(t=r=f,l=e),i>f&&f>r&&(r=f,n=e)}),t>r&&(l=n)),f=e.gridheight[l]||e.gridheight[0]||e.gridheight,s=e.gridwidth[l]||e.gridwidth[0]||e.gridwidth,h=i/s,h=h>1?1:h,f=Math.round(h*f),"fullscreen"==e.sliderLayout){var u=(e.c.width(),jQuery(window).height());if(void 0!=e.fullScreenOffsetContainer){var c=e.fullScreenOffsetContainer.split(",");if (c) jQuery.each(c,function(e,i){u=jQuery(i).length>0?u-jQuery(i).outerHeight(!0):u}),e.fullScreenOffset.split("%").length>1&&void 0!=e.fullScreenOffset&&e.fullScreenOffset.length>0?u-=jQuery(window).height()*parseInt(e.fullScreenOffset,0)/100:void 0!=e.fullScreenOffset&&e.fullScreenOffset.length>0&&(u-=parseInt(e.fullScreenOffset,0))}f=u}else void 0!=e.minHeight&&f<e.minHeight&&(f=e.minHeight);e.c.closest(".rev_slider_wrapper").css({height:f})
+                }catch(d){console.log("Failure at Presize of Slider:"+d)}
+            };
     </script>
 
     <style>
@@ -733,26 +733,7 @@
     class="home page-template page-template-template-front-page-slider page-template-template-front-page-slider-php page page-id-6 siteorigin-panels siteorigin-panels-before-js siteorigin-panels-home">
     <div class="boxed-container">
 
-        <div class="top">
-            <div class="container">
-                <!-- Top Tagline from WordPress -->
-                <div class="top__tagline">
-                    All about Legend logistics </div>
-                <!-- Top Menu -->
-                <nav class="top__menu" role="navigation" aria-label="Top Menu">
-                    <ul id="menu-top-menu" class="top-navigation  js-dropdown" role="menubar">
-                        <li id="menu-item-793"
-                            class="menu-item menu-item-type-custom menu-item-object-custom menu-item-793">
-                        <li id="menu-item-824"
-                            class="menu-item menu-item-type-post_type menu-item-object-page menu-item-824"><a
-                                href="{{ url('/about-us') }}">About Us</a></li>
-                        <li id="menu-item-791"
-                            class="menu-item menu-item-type-post_type menu-item-object-page menu-item-791"><a
-                                href="#">Contact Us</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
+        @include('partials.nav-upper')
 
         <div class="header__container">
             <div class="container">
