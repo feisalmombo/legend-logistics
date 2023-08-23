cf325023ab3f64dd22263db539547c932c66d1d3
feisalmombo <feisalmombo29@gmail.com>
2023-08-23 11:46:00 +0300

    add chat blade file on partial folder

---
+++ resources/views/partials/chat.blade.php
@@ -0,0 +1,41 @@
+<div id="qlwapp" class="qlwapp-free qlwapp-button qlwapp-bottom-right qlwapp-all qlwapp-rounded">
+    <div class="qlwapp-container">
+        <div class="qlwapp-box">
+            <div class="qlwapp-header">
+                <i class="qlwapp-close" data-action="close">&times;</i>
+                <div class="qlwapp-description">
+                    <h3>Hello!</h3>
+                    <p>chat on WhatsApp or send us an email to <a href="mailto:info@legendlogistics.co.tz"
+                            style="text-decoration: none; color: white">info@legendlogistics.co.tz&nbsp;</a></p>
+                </div>
+            </div>
+            <div class="qlwapp-body">
+                <a class="qlwapp-account" data-action="open" data-phone="+255776666663" data-message="Hello!"
+                    href="javascript:void(0);" target="_blank">
+                    <div class="qlwapp-avatar">
+                        <div class="qlwapp-avatar-container">
+                            <img alt="Theoneste " data-src="{{asset('gfl/images/whatsapp-image.png')}}"
+                                class="lazyload"
+                                src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==">
+                        </div>
+                    </div>
+                    <div class="qlwapp-info">
+                        <span class="qlwapp-label">Support</span>
+                        <span class="qlwapp-name">Theoneste </span>
+                    </div>
+                </a>
+            </div>
+            <div class="qlwapp-footer">
+                <p>Call us to <a href="tel://+255776666663">+255 776 666 663</a>&nbsp;from <em>0:00hs</em> a
+                    <em>24:00hs</em>
+                </p>
+            </div>
+        </div>
+        <a class="qlwapp-toggle" data-action="box" data-phone="+255776666663" data-message="Hello!" href="#"
+            target="_blank">
+            <i class="qlwapp-icon qlwapp-whatsapp-icon"></i>
+            <i class="qlwapp-close" data-action="close">&times;</i>
+            <span class="qlwapp-text">Whatsapp Legend Logistics</span>
+        </a>
+    </div>
+</div>
