fffb6303dff6763dbbf4515b7daa931c3b108d2e
feisalmombo <feisalmombo29@gmail.com>
2023-08-23 11:53:40 +0300

    WIP

--- resources/views/pages/about-us.blade.php
+++ resources/views/pages/about-us.blade.php
@@ -850,47 +850,8 @@
 
     </div><!-- end of .boxed-container -->
 
-    <div id="qlwapp" class="qlwapp-free qlwapp-button qlwapp-bottom-right qlwapp-all qlwapp-rounded">
-        <div class="qlwapp-container">
-            <div class="qlwapp-box">
-                <div class="qlwapp-header">
-                    <i class="qlwapp-close" data-action="close">&times;</i>
-                    <div class="qlwapp-description">
-                        <h3>Hello!</h3>
-                        <p>chat on WhatsApp or send us an email to <a
-                                href="mailto:info@umojalogistics.co.rw">info@umojalogistics.co.rw </a></p>
-                    </div>
-                </div>
-                <div class="qlwapp-body">
-                    <a class="qlwapp-account" data-action="open" data-phone="+250783940444" data-message="Hello!"
-                        href="javascript:void(0);" target="_blank">
-                        <div class="qlwapp-avatar">
-                            <div class="qlwapp-avatar-container">
-                                <img alt="Theoneste "
-                                    data-src="https://umojalogistics.co.rw/wp-content/uploads/2019/07/bossbusinesspeoplebusinessmanlawyerownerpersonicon-1320086651899415591.png"
-                                    class="lazyload"
-                                    src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==">
-                            </div>
-                        </div>
-                        <div class="qlwapp-info">
-                            <span class="qlwapp-label">Support</span>
-                            <span class="qlwapp-name">Theoneste </span>
-                        </div>
-                    </a>
-                </div>
-                <div class="qlwapp-footer">
-                    <p>Call us to <a href="tel://+2507224455241">+250 7224455241</a> from <em>0:00hs</em> a
-                        <em>24:00hs</em></p>
-                </div>
-            </div>
-            <a class="qlwapp-toggle" data-action="box" data-phone="+250724455241" data-message="Hello!" href="#"
-                target="_blank">
-                <i class="qlwapp-icon qlwapp-whatsapp-icon"></i>
-                <i class="qlwapp-close" data-action="close">&times;</i>
-                <span class="qlwapp-text">Whatsapp Umoja Logistics</span>
-            </a>
-        </div>
-    </div>
+    @include('partials.chat')
+
     <script type='text/javascript'>
         /* <![CDATA[ */
 var wpcf7 = {"apiSettings":{"root":"https:\/\/umojalogistics.co.rw\/index.php\/wp-json\/contact-form-7\/v1","namespace":"contact-form-7\/v1"}};
