ee1317aae08b5d4c7cc442c8c82d86a5a4377045
feisalmombo <feisalmombo29@gmail.com>
2024-03-14 16:19:40 +0300

    WIP

--- resources/views/partials/header-container.blade.php
+++ resources/views/partials/header-container.blade.php
@@ -3,10 +3,10 @@
         <header class="header" role="banner">
             <div class="header__logo">
                 <a href="#">
-                    <img alt="Legend Logistics" data-srcset="{{asset('gfl/images/logo/legend-logo.png')}}" width="218" height="40"
-                        data-src="3" class="img-responsive lazyload" src="#" alt="Legend Logistics Company Limited">
+                    {{-- <img alt="Legend Logistics" data-srcset="{{asset('gfl/images/logo/legend-logo.png')}}" width="218" height="40"
+                        data-src="3" class="img-responsive lazyload" src="#" alt="Legend Logistics Company Limited"> --}}
                 </a>
-                {{-- <h1>Legend Logistics</h1> --}}
+                <h1>Legend Logistics</h1>
                 <button type="button" class="navbar-toggle" data-toggle="collapse"
                     data-target="#cargopress-navbar-collapse">
                     <span class="navbar-toggle__text">MENU</span>
